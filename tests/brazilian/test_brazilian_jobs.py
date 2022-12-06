eb27f2077ecec0980120081eb2f21347da34e219
matheusnff85 <math.marinho154@gmail.com>
2022-12-06 19:20:41 -0300

    feat: implementa teste para o brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    assert read_brazilian_file("tests/mocks/brazilians_jobs.csv")[1] == {
+        "title": "Motorista",
+        "salary": "3000",
+        "type": "full time",
+    }
