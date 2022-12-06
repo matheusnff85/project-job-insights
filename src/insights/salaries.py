04b2b8b7439b3652de7ff2854802968489aec4d6
matheusnff85 <math.marinho154@gmail.com>
2022-12-06 16:20:33 -0300

    feat: funçao get max salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,15 @@
 from typing import Union, List, Dict
+from .jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    read_data = read(path)
+    set_list = set()
+    for item in read_data:
+        if item["salary"] != "":
+            set_list.add(item["salary"])
+    salary_list = list(set_list)
+    return max(salary_list)
 
 
 def get_min_salary(path: str) -> int:
