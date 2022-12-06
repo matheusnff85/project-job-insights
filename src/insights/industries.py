4a52e2883e6f14aab0922fbb4f7805abe9bff39e
matheusnff85 <math.marinho154@gmail.com>
2022-12-06 15:43:41 -0300

    feat: implementa função de unique industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,14 @@
 from typing import List, Dict
+from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    read_data = read(path)
+    set_list = set()
+    for item in read_data:
+        set_list.add(item["industry"])
+    job_list = list(set_list)
+    return job_list
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
